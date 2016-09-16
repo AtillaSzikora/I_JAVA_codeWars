0ed6c6b1254c845f1c8cb6b7adfc20a839d0ad6b
AtillaSzikora <szikora.atilla@gmail.com>
2016-09-16 14:59:27 +0200

    Empty find even index kata added with tests. Fixed.

---
+++ src/Kata.java
@@ -0,0 +1,16 @@
+/*
+You are going to be given an array of integers. Your job is to take that array and find an index N
+where the sum of the integers to the left of N is equal to the sum of the integers to the right of N.
+If there is no index that would make this happen, return -1.
+ */
+
+class Kata {
+    static int findEvenIndex(int[] arr) {
+        for (int i = 1; i < arr.length - 1; i++) {
+            if () {
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+}
