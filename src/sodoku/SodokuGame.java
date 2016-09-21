80e78274d95713d80261c8b3919f115ba6c0945a
AtillaSzikora <szikora.atilla@gmail.com>
2016-09-21 11:25:20 +0200

    Sodoku gamefield is member var now.

--- src/sodoku/SodokuGame.java
+++ src/sodoku/SodokuGame.java
@@ -3,42 +3,41 @@ package sodoku;
 import java.util.*;
 
 public class SodokuGame {
-    static int[][] gameField = new int[][] {
-            { 5, 3, 4, 6, 7, 8, 9, 1, 2 },
-            { 6, 7, 2, 1, 9, 5, 3, 4, 8 },
-            { 1, 9, 8, 3, 4, 2, 5, 6, 7 },
-            { 8, 5, 9, 7, 6, 1, 4, 2, 3 },
-            { 4, 2, 6, 8, 5, 3, 7, 9, 1 },
-            { 7, 1, 3, 9, 2, 4, 8, 5, 6 },
-            { 9, 6, 1, 5, 3, 7, 2, 8, 4 },
-            { 2, 8, 7, 4, 1, 9, 6, 3, 5 },
-            { 3, 4, 5, 2, 8, 6, 1, 7, 9 } };
-
     public static void main(String[] args) {
-        System.out.println(isTableValid());
+        int[][] gameField = new int[][] {
+                { 5, 3, 4, 6, 7, 8, 9, 1, 2 },
+                { 6, 7, 2, 1, 9, 5, 3, 4, 8 },
+                { 1, 9, 8, 3, 4, 2, 5, 6, 7 },
+                { 8, 5, 9, 7, 6, 1, 4, 2, 3 },
+                { 4, 2, 6, 8, 5, 3, 7, 9, 1 },
+                { 7, 1, 3, 9, 2, 4, 8, 5, 6 },
+                { 9, 6, 1, 5, 3, 7, 2, 8, 4 },
+                { 2, 8, 7, 4, 1, 9, 6, 3, 5 },
+                { 3, 4, 5, 2, 8, 6, 1, 7, 9 } };
+        System.out.println(isTableValid(gameField));
     }
 
-    private static int[] getBlock(int blockRowIndex, int blockColIndex) {
+    private static int[] getBlock(int[][] table, int blockRowIndex, int blockColIndex) {
         int[] resultBlock = new int[9];
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                resultBlock[i * 3 + j] = gameField[blockRowIndex * 3 + i][blockColIndex * 3 + j];
+                resultBlock[i * 3 + j] = table[blockRowIndex * 3 + i][blockColIndex * 3 + j];
             }
         }
         return resultBlock;
     }
 
-    public static boolean isTableValid() {
-        boolean rows = areRowsValid();
-        boolean cols = areColumnsValid();
-        boolean blocks = areBlocksValid();
+    public static boolean isTableValid(int[][] table) {
+        boolean rows = areRowsValid(table);
+        boolean cols = areColumnsValid(table);
+        boolean blocks = areBlocksValid(table);
         return rows && cols && blocks;
     }
 
-    private static boolean areBlocksValid() {
+    private static boolean areBlocksValid(int[][] table) {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                int[] block = getBlock(j, i);
+                int[] block = getBlock(table, j, i);
                 if (containsDuplicates(block)) {
                     return false;
                 }
@@ -56,8 +55,8 @@ public class SodokuGame {
         return hashSet.size() != array.length;
     }
 
-    private static boolean areRowsValid() {
-        for (int[] row: gameField) {
+    private static boolean areRowsValid(int[][] table) {
+        for (int[] row: table) {
             if (containsDuplicates(row)) {
                 return false;
             }
@@ -65,9 +64,9 @@ public class SodokuGame {
         return true;
     }
 
-    private static boolean areColumnsValid() {
-        for (int i = 0; i < gameField[0].length; i++) {
-            int[] col = getColumn(i);
+    private static boolean areColumnsValid(int[][] table) {
+        for (int i = 0; i < table[0].length; i++) {
+            int[] col = getColumn(table, i);
             if (containsDuplicates(col)) {
                 return false;
             }
@@ -75,10 +74,10 @@ public class SodokuGame {
         return true;
     }
 
-    private static int[] getColumn(int columnIndex) {
-        int[] resultColumn = new int[gameField.length];
-        for (int i = 0; i < gameField.length; i++) {
-            int[] row = gameField[i];
+    private static int[] getColumn(int[][] table, int columnIndex) {
+        int[] resultColumn = new int[table.length];
+        for (int i = 0; i < table.length; i++) {
+            int[] row = table[i];
             resultColumn[i] = row[columnIndex];
         }
         return resultColumn;
